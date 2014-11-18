23d436fcdd3064d5161cd9b21b02ded659ab3d5d
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2014-11-18 15:58:45 -0700

    gets() searches for objects and raises proper exceptions if exactly one result is not found.

--- src/ldapobjectmanager.py
+++ src/ldapobjectmanager.py
@@ -6,7 +6,19 @@ class auth():
 class LDAPObjectManager():
 
     def __init__(self, uri, authtype, **kwargs):
-        pass
+        self.ldo = ldap.initialize(uri)
 
-    def gets(self, base, filter):
-        raise RuntimeError
+    def _stripReferences(self, ldif):
+        return filter(lambda x: x[0] is not None, ldif)
+
+    def gets(self, sbase, sfilter):
+        ldif = self.ldo.search_ext_s(sbase, ldap.SCOPE_SUBTREE, sfilter)
+        result = self._stripReferences(ldif)
+        if not result:
+            raise RuntimeError("No results found for single-object query: \
+base %s filter: %s" %(sbase, sfilter))
+        if len(result) > 1:
+            raise RuntimeError("Too many results found for single-object \
+query: base: %s filter: %s results: %s" %(sbase, sfilter, \
+                [r[0] for r in result]))
+        return result[0]
--- tests/test_ldapobjectmanager.py
+++ tests/test_ldapobjectmanager.py
@@ -6,17 +6,43 @@ import src.ldapobjectmanager
 class TestLOMGetMethods(unittest.TestCase):
 
     def setUp(self):
-        self.uri = 'ldaps://foo.bar:636'
-        self.lom = src.ldapobjectmanager.LDAPObjectManager(self.uri,
-            src.ldapobjectmanager.auth.kerb)
-
-    # tests for LOM.gets()
+        pass
     
-    def testGetsEmptyException(self, mock_ldap):
-        ldo = mock_ldap.ldapobject.LDAPObject(self.uri)
+    def testGets(self, mock_ldap):
+        uri = 'ldaps://foo.bar:636'
+        ldo = mock_ldap.ldapobject.LDAPObject(uri)
         mock_ldap.initialize.return_value = ldo
+        lom = src.ldapobjectmanager.LDAPObjectManager(uri,
+            src.ldapobjectmanager.auth.kerb)
 
         # if gets() fails to find an object, it should throw an exception
         ldo.search_ext_s.return_value = []
         with self.assertRaises(RuntimeError) as err:
-            self.lom.gets("", "")
+            lom.gets("", "")
+
+        # sometimes references are included in the result
+        # these have no DN and should be discarded from the result
+        ldo.search_ext_s.return_value = [(None, ['ldaps://foo.bar/cn=ref'])]
+        with self.assertRaises(RuntimeError) as err:
+            lom.gets("", "")
+
+        # if gets() finds > 1 object, it should throw an exception
+        ldo.search_ext_s.return_value = [
+            ('CN=fred,OU=People,DC=foo,DC=bar', {'name': ['fred']}),
+            ('CN=george,OU=People,DC=foo,DC=bar', {'name': ['george']})
+            ]
+        with self.assertRaises(RuntimeError) as err:
+            lom.gets("", "(|(name=fred)(name=george))")
+
+        # if gets() finds exactly 1 object, it should return that object
+        expectedresult = ('CN=alice,OU=People,DC=foo,DC=bar', {'name': ['alice']})
+        ldo.search_ext_s.return_value = [expectedresult]
+        actualresult = lom.gets("", "name=alice")
+        self.assertEqual(expectedresult, actualresult)
+
+        # repeat with reference in result
+        expectedresult = ('CN=alice,OU=People,DC=foo,DC=bar', {'name': ['alice']})
+        ldo.search_ext_s.return_value = [expectedresult,
+            (None, ['ldaps://foo.bar/cn=ref'])]
+        actualresult = lom.gets("", "name=alice")
+        self.assertEqual(expectedresult, actualresult)
