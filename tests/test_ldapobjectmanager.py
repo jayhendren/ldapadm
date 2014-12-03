7e9b1d763eaa8ee4e14b2cff542fa49e122d039a
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2014-12-03 16:35:57 -0700

    heavy refactor of test code

--- tests/test_ldapobjectmanager.py
+++ tests/test_ldapobjectmanager.py
@@ -1,116 +1,116 @@
 import mock
 import unittest
 import src.ldapobjectmanager
+from src.ldapobjectmanager import LDAPObjectManager, auth
 
-@mock.patch('src.ldapobjectmanager.ldap', autospec=True)
-class TestLOMInitializationAndOptions(unittest.TestCase):
+uri = 'ldaps://foo.bar:636'
 
-    def testAuth(self, mock_ldap):
-        uri = 'ldaps://foo.bar:636'
-        def getNewLDOandLOM(auth, **kwargs):
-            ldo = mock_ldap.ldapobject.LDAPObject(uri)
-            mock_ldap.initialize.return_value = ldo
-            lom = src.ldapobjectmanager.LDAPObjectManager(uri, auth, **kwargs)
-            return ldo, lom
+def person(name):
+    return ('CN={},OU=People,DC=foo,DC=bar'.format(name), {'name': [name]})
 
-        # no auth
-        ldo, lom = getNewLDOandLOM(src.ldapobjectmanager.auth.noauth)
-        self.assertEqual(ldo.simple_bind_s.call_args_list, [])
-        self.assertEqual(ldo.sasl_interactive_bind_s.call_args_list, [])
+def reference():
+    return (None, ['ldaps://foo.bar/cn=ref'])
 
-        # simple auth
+class LOMTestCase(unittest.TestCase):
+
+    def setUp(self):
+        patcher = mock.patch('src.ldapobjectmanager.ldap', autospec=True)
+        self.mock_ldap = patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def getNewLDOandLOM(self, auth, **kwargs):
+        ldo = self.mock_ldap.ldapobject.LDAPObject(uri)
+        self.mock_ldap.initialize.return_value = ldo
+        lom = LDAPObjectManager(uri, auth, **kwargs)
+        return ldo, lom
+
+    def assert_no_calls(self, method):
+        self.assertEqual(method.call_args_list, [])
+
+class TestLOMInitializationAndOptions(LOMTestCase):
+
+    def testNoAuth(self):
+        ldo, lom = self.getNewLDOandLOM(auth.noauth)
+        self.assert_no_calls(ldo.simple_bind_s)
+        self.assert_no_calls(ldo.sasl_interactive_bind_s)
+
+    def testSimpleAuth(self):
         user = 'foo'
         password = 'bar'
-        ldo, lom = getNewLDOandLOM(src.ldapobjectmanager.auth.simple,
-            user=user, password=password)
-        self.assertEqual(ldo.simple_bind_s.call_args_list,
-            [((user, password),)])
+        ldo, lom = self.getNewLDOandLOM(auth.simple, user=user, password=password)
+        ldo.simple_bind_s.assert_called_once_with(user, password)
 
-        # kerb auth
+    def testKerbAuth(self):
         sasl = mock.MagicMock()
-        mock_ldap.sasl.gssapi.return_value = sasl
-        ldo, lom = getNewLDOandLOM(src.ldapobjectmanager.auth.kerb)
-        self.assertEqual(ldo.sasl_interactive_bind_s.call_args_list,
-            [(('', sasl),)])
+        self.mock_ldap.sasl.gssapi.return_value = sasl
+        ldo, lom = self.getNewLDOandLOM(auth.kerb)
+        ldo.sasl_interactive_bind_s.assert_called_once_with('', sasl)
 
-    def testOptions(self, mock_ldap):
-        uri = 'ldaps://foo.bar:636'
-        def addOption(**kwargs):
-            ldo = mock_ldap.ldapobject.LDAPObject(uri)
-            mock_ldap.initialize.return_value = ldo
+    def testAddInvalidOptionThrowsException(self):
+        with self.assertRaises(AttributeError):
+            ldo, lom = self.getNewLDOandLOM(auth.kerb, OPT_BOGUS=1)
+
+    def testOptionKwargsAreSetOnConnectionObject(self):
+        def assertOptionsAdded(**kwargs):
+            ldo, lom = self.getNewLDOandLOM(auth.noauth, **kwargs)
             for key, value in kwargs.items():
-                if not hasattr(mock_ldap, key):
-                    with self.assertRaises(AttributeError):
-                        lom = src.ldapobjectmanager.LDAPObjectManager(uri,
-                            src.ldapobjectmanager.auth.noauth, **{key:value})
-                else:
-                    lom = src.ldapobjectmanager.LDAPObjectManager(uri,
-                        src.ldapobjectmanager.auth.noauth, **{key:value})
-                    self.assertEqual(ldo.set_option.call_args, 
-                        ((getattr(mock_ldap, key), value),))
-        addOption(OPT_X_TLS=1, OPT_BOGUS=1, OPT_URI="ldaps://baz.bar")
+                ldo.set_option.assert_any_call(getattr(self.mock_ldap, key),
+                                               value)
 
-@mock.patch('src.ldapobjectmanager.ldap', autospec=True)
-class TestLOMGetMethods(unittest.TestCase):
+        assertOptionsAdded(OPT_X_TLS=1)
+        assertOptionsAdded(OPT_REFERRALS=0, OPT_URI="ldaps://baz.bar")
 
-    def testGets(self, mock_ldap):
-        uri = 'ldaps://foo.bar:636'
-        ldo = mock_ldap.ldapobject.LDAPObject(uri)
-        mock_ldap.initialize.return_value = ldo
-        lom = src.ldapobjectmanager.LDAPObjectManager(uri,
-            src.ldapobjectmanager.auth.kerb)
+class LOMGetMethodsTestCase(LOMTestCase):
 
-        # if gets() fails to find an object, it should throw an exception
-        ldo.search_ext_s.return_value = []
-        with self.assertRaises(RuntimeError) as err:
-            lom.gets("", "")
+    def setUp(self):
+        super(LOMGetMethodsTestCase, self).setUp()
+        self.ldo, self.lom = self.getNewLDOandLOM(auth.kerb)
 
+class TestLOMGets(LOMGetMethodsTestCase):
+
+    def testGetsThrowsExceptionForNoResultsFound(self):
+        self.ldo.search_ext_s.return_value = []
+        with self.assertRaises(RuntimeError):
+            self.lom.gets("", "")
+
+    def testGetsThrowsExceptionForOnlyReferencesFound(self):
         # sometimes references are included in the result
         # these have no DN and should be discarded from the result
-        ldo.search_ext_s.return_value = [(None, ['ldaps://foo.bar/cn=ref'])]
-        with self.assertRaises(RuntimeError) as err:
-            lom.gets("", "")
+        self.ldo.search_ext_s.return_value = [(None, ['ldaps://foo.bar/cn=ref'])]
+        with self.assertRaises(RuntimeError):
+            self.lom.gets("", "")
 
-        # if gets() finds > 1 object, it should throw an exception
-        ldo.search_ext_s.return_value = [
-            ('CN=fred,OU=People,DC=foo,DC=bar', {'name': ['fred']}),
-            ('CN=george,OU=People,DC=foo,DC=bar', {'name': ['george']})
-            ]
-        with self.assertRaises(RuntimeError) as err:
-            lom.gets("", "(|(name=fred)(name=george))")
+    def testGetsSuccessfullyReturnsExactlyOneObject(self):
+        alice = person('alice')
+        self.ldo.search_ext_s.return_value = [alice]
+        self.assertEqual(alice, self.lom.gets("", "name=alice"))
 
-        # if gets() finds exactly 1 object, it should return that object
-        expectedresult = ('CN=alice,OU=People,DC=foo,DC=bar', {'name': ['alice']})
-        ldo.search_ext_s.return_value = [expectedresult]
-        actualresult = lom.gets("", "name=alice")
-        self.assertEqual(expectedresult, actualresult)
+    def testGetsSuccessfullyReturnsExactlyOneObject(self):
+        bob = person('bob')
+        self.ldo.search_ext_s.return_value = [bob, reference()]
+        self.assertEqual(bob, self.lom.gets("", "name=bob"))
 
-        # repeat with reference in result
-        expectedresult = ('CN=alice,OU=People,DC=foo,DC=bar', {'name': ['alice']})
-        ldo.search_ext_s.return_value = [expectedresult,
-            (None, ['ldaps://foo.bar/cn=ref'])]
-        actualresult = lom.gets("", "name=alice")
-        self.assertEqual(expectedresult, actualresult)
+    def testGetsThrowsExceptionWhenMultipleResultsFound(self):
+        expectedresult = [person('fred'), person('george')]
+        self.ldo.search_ext_s.return_value = expectedresult
+        with self.assertRaises(RuntimeError):
+            self.lom.gets("", "")
 
-    def testGetm(self, mock_ldap):
-        uri = 'ldaps://foo.bar:636'
-        ldo = mock_ldap.ldapobject.LDAPObject(uri)
-        mock_ldap.initialize.return_value = ldo
-        lom = src.ldapobjectmanager.LDAPObjectManager(uri,
-            src.ldapobjectmanager.auth.kerb)
+        self.assertEqual(expectedresult, self.lom.getm("", ""))
 
-        expectedresult = [
-            ('CN=fred,OU=People,DC=foo,DC=bar', {'name': ['fred']}),
-            ('CN=george,OU=People,DC=foo,DC=bar', {'name': ['george']})
-            ]
-        ldo.search_ext_s.return_value = expectedresult
-        actualresult = lom.getm("", "(|(name=fred)(name=george))")
-        self.assertEqual(expectedresult, actualresult)
+class TestLOMGetm(LOMGetMethodsTestCase):
 
-        # repeat with reference in result
-        alice = ('CN=alice,OU=People,DC=foo,DC=bar', {'name': ['alice']})
-        reference = (None, ['ldaps://foo.bar/cn=ref'])
-        ldo.search_ext_s.return_value = [reference, alice, alice, reference,
-            reference, alice, alice, reference, alice, reference]
-        actualresult = lom.getm("", "name=alice")
-        self.assertEqual([alice, alice, alice, alice, alice], actualresult)
+    def testGetmSuccessfullyReturnsMultipleResults(self):
+        expectedresult = [person('fred'), person('george')]
+        self.ldo.search_ext_s.return_value = expectedresult
+        self.assertEqual(expectedresult, self.lom.getm("", ""))
+
+    def testGetmRemovesReferenceFromResult(self):
+        susie = person('susie')
+        ref = reference()
+        self.ldo.search_ext_s.return_value = [
+            ref, susie, susie, ref,
+            ref, susie, susie, ref, susie, ref
+        ]
+        actualresult = self.lom.getm("", "name=susie")
+        self.assertEqual([susie] * 5, actualresult)
