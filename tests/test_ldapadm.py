1f9fb5fef05ee815d51c6c3b653b70091200b259
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2015-01-15 14:13:36 -0700

    fixed help message tests

--- tests/test_ldapadm.py
+++ tests/test_ldapadm.py
@@ -2,19 +2,20 @@ import os
 import subprocess
 import unittest
 
-def runLdapadm(args):
+def runLdapadm(*args):
     # a CalledProcessError will be raised if the exit code is not zero
     # this function returns stdout of ldapadm
     proj_root_dir = os.path.split(os.path.dirname(\
         os.path.realpath(__file__)))[0]
-    return subprocess.check_output(os.path.join(proj_root_dir,
-        'scripts/ldapadm.py'))
+    cmd = [os.path.join(proj_root_dir, 'scripts/ldapadm.py')] + list(args)
+    return subprocess.check_output(cmd)
 
 class LdapadmBasicTests(unittest.TestCase):
 
     def testCanRunLdapadm(self):
-        runLdapadm("")
+        with self.assertRaises(subprocess.CalledProcessError):
+            runLdapadm()
 
     def testHelpSwitchesExitCodeZeroAndProduceOutput(self):
-        self.assertRegexpMatches(runLdapadm("-h"), r'Usage:')
-        self.assertRegexpMatches(runLdapadm("--help"), r'Usage:')
+        self.assertRegexpMatches(runLdapadm("-h"), r'usage:')
+        self.assertRegexpMatches(runLdapadm("--help"), r'usage:')
