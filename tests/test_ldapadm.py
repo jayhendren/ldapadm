ab21273de6dfeef648ed41b61b25789859e39543
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2015-01-15 16:12:29 -0700

    use popen instead of check_output for better handling of stdout & stderr data

--- tests/test_ldapadm.py
+++ tests/test_ldapadm.py
@@ -4,18 +4,30 @@ import unittest
 
 def runLdapadm(*args):
     # a CalledProcessError will be raised if the exit code is not zero
-    # this function returns stdout of ldapadm
+    # this function returns tuple (stdout, stderr) of ldapadm output
     proj_root_dir = os.path.split(os.path.dirname(\
         os.path.realpath(__file__)))[0]
     cmd = [os.path.join(proj_root_dir, 'scripts/ldapadm.py')] + list(args)
-    return subprocess.check_output(cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    result =  proc.communicate()
+    if proc.returncode != 0: raise subprocess.CalledProcessError('', '')
+    return result
 
 class LdapadmBasicTests(unittest.TestCase):
 
-    def testCanRunLdapadm(self):
+    def testLdapadmCalledWithoutArgumentsReturnsError(self):
         with self.assertRaises(subprocess.CalledProcessError):
             runLdapadm()
 
     def testHelpSwitchesExitCodeZeroAndProduceOutput(self):
-        self.assertRegexpMatches(runLdapadm("-h"), r'usage:')
-        self.assertRegexpMatches(runLdapadm("--help"), r'usage:')
+        self.assertRegexpMatches(runLdapadm("-h")[0], r'usage:')
+        self.assertRegexpMatches(runLdapadm("--help")[0], r'usage:')
+
+class LdapadmGetTests(unittest.TestCase):
+
+    def testGetWithBadArgumentsReturnsError(self):
+        with self.assertRaises(subprocess.CalledProcessError):
+            runLdapadm('get')
+        with self.assertRaises(subprocess.CalledProcessError):
+            runLdapadm('get', 'bogus')
