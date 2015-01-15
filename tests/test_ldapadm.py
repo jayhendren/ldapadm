fa7a79dd82d650e044a449614ea35a184172e027
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2015-01-15 12:26:58 -0700

    wrote tests for help switches

--- tests/test_ldapadm.py
+++ tests/test_ldapadm.py
@@ -2,9 +2,19 @@ import os
 import subprocess
 import unittest
 
+def runLdapadm(args):
+    # a CalledProcessError will be raised if the exit code is not zero
+    # this function returns stdout of ldapadm
+    proj_root_dir = os.path.split(os.path.dirname(\
+        os.path.realpath(__file__)))[0]
+    return subprocess.check_output(os.path.join(proj_root_dir,
+        'scripts/ldapadm.py'))
+
 class LdapadmBasicTests(unittest.TestCase):
 
-    def testStub(self):
-        proj_root_dir = os.path.split(os.path.dirname(\
-            os.path.realpath(__file__)))[0]
-        subprocess.call(os.path.join(proj_root_dir, 'scripts/ldapadm'))
+    def testCanRunLdapadm(self):
+        runLdapadm("")
+
+    def testHelpSwitchesExitCodeZeroAndProduceOutput(self):
+        self.assertRegexpMatches(runLdapadm("-h"), r'Usage:')
+        self.assertRegexpMatches(runLdapadm("--help"), r'Usage:')
